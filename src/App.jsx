691e5697d9892acc51ed39f27e81958697569987
rahul <nameisrahulsenthakuri@gmail.com>
2025-05-25 08:10:05 +0545

    completed-day-5

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className='container'>
       <div className='first'>
-
         <div className='login'>
           <img className='image1' src='https://admin.workplanx.dev/beta-mark.svg'>
           </img>
@@ -12,50 +11,33 @@ function App() {
             <img className='image2' src='https://admin.workplanx.dev/new-workplanXlogo.svg'>
             </img>
             <div className='log'>Login to your  account</div>
-
             <div className=''>
               <p className='email'>Email</p>
-              <input className='email-address' placeholder='enter your email address'></input>
+              <input className='email-address' placeholder='Enter your email address'></input>
             </div>
-
-
             <div className='ram'>
               <div className='text3'> Use mobile instead?</div>
               <button className='button'>log in to your account</button>
-
-
-
             </div>
-
           </div>
-
-
         </div>
       </div>
       <div className='second'>
-
         <div className='overlay'>
           <div>
             <img className='image3' src='https://admin.workplanx.dev/doubleQuote.svg'>
             </img>
-            <p className='second-text'>Delegation made easy</p>
-
+            <p className='second-text'>Delegation made <br /> easy</p>
           </div>
+          <img className='image4' src='https://admin.workplanx.dev/white-logo.svg'>
+          </img>
 
         </div>
-
         <video className='video' muted
           playsInline
           src="https://admin.workplanx.dev/login-info-video.mp4" autoPlay={true} loop>
-
         </video>
-
-
-
-
-
       </div>
-
     </div>
   )
 }
